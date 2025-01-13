0dad4b1645b5c73bd6bf64034b56f9928ad7315e
RaizanKA <raizankhairulanam@gmail.com>
2025-01-13 15:09:08 +0700

    Add files via upload
    Signed-off-by: RaizanKA <raizankhairulanam@gmail.com>

---
+++ gokil.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Responsive Form</title>
+  <!-- Tambahkan link Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <div class="container mt-4">
+    <form>
+      <div class="row">
+        <div class="col-12 col-md-6 mb-3">
+          <label for="input1" class="form-label">Input 1</label>
+          <input type="text" id="input1" class="form-control" placeholder="Input 1">
+        </div>
+        <div class="col-12 col-md-6 mb-3">
+          <label for="input2" class="form-label">Input 2</label>
+          <input type="text" id="input2" class="form-control" placeholder="Input 2">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-6 mb-3">
+          <label for="input3" class="form-label">Input 3</label>
+          <input type="text" id="input3" class="form-control" placeholder="Input 3">
+        </div>
+        <div class="col-12 col-md-6 mb-3">
+          <label for="input4" class="form-label">Input 4</label>
+          <input type="text" id="input4" class="form-control" placeholder="Input 4">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-6 mb-3">
+          <label for="input5" class="form-label">Input 5</label>
+          <input type="text" id="input5" class="form-control" placeholder="Input 5">
+        </div>
+        <div class="col-12 col-md-6 mb-3">
+          <label for="input6" class="form-label">Input 6</label>
+          <input type="text" id="input6" class="form-control" placeholder="Input 6">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-6 mb-3">
+          <label for="input7" class="form-label">Input 7</label>
+          <input type="text" id="input7" class="form-control" placeholder="Input 7">
+        </div>
+        <div class="col-12 col-md-6 mb-3">
+          <label for="input8" class="form-label">Input 8</label>
+          <input type="text" id="input8" class="form-control" placeholder="Input 8">
+        </div>
+      </div>
+    </form>
+  </div>
+
+  <!-- Tambahkan link Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
